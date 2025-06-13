6a25597beb1d1ffcdd41cfa3dfe1ff7b74119e6d
Juanramos17 <juanramoscaro@gmail.com>
2025-06-13 10:17:33 +0200

    Cambios

--- public/index.php
+++ public/index.php
@@ -17,4 +17,8 @@ require __DIR__.'/../vendor/autoload.php';
 /** @var Application $app */
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-$app->handleRequest(Request::capture());
+$response = $app->handle(
+    $request = Request::capture()
+);
+
+$response->send();
